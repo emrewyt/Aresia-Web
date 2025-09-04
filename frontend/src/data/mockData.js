e9212a1bf4e229c81f526534b2c6a7fa01e588af
Emre <umutemreyilman66@gmail.com>
2025-09-05 02:10:40 +0300

    Update mockData.js
    Resimler Fixlendi

--- frontend/src/data/mockData.js
+++ frontend/src/data/mockData.js
@@ -30,154 +30,154 @@ export const mockGames = [
   {
     id: 5,
     title: "Portal 2",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/620/header.jpg",
     hash: 4,
     link: "https://ay.live/Ar9K"
   },
   {
     id: 6,
     title: "Lethal Company",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/1966720/header.jpg",
     hash: 5,
     link: "https://ay.live/05pJ"
   },
   {
     id: 7,
     title: "Human Fall Flat",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/477160/header.jpg",
     hash: 6,
     link: "https://ay.live/TmFDP"
   },
   {
     id: 8,
     title: "Prisoners",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/2725200/header.jpg",
     hash: 7,
     link: ""
   },
   {
     id: 9,
     title: "Pummel Party",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/880940/header.jpg",
     hash: 8,
     link: "https://ay.live/P2Z7m"
   },
   {
     id: 10,
     title: "Who's Your Daddy",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/427730/header.jpg",
     hash: 9,
     link: "https://ay.live/1onbo"
   },
   {
     id: 11,
     title: "Werewolf Party",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/2920510/header.jpg",
     hash: 10,
     link: "https://ay.live/7MBj"
   },
   {
     id: 12,
     title: "Astroneer",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/361420/header.jpg",
     hash: 11,
     link: "https://ay.live/1UZB0"
   },
   {
     id: 13,
     title: "Content Warning",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/2881650/header.jpg",
     hash: 12,
     link: "https://ay.live/yYwWs"
   },
   {
     id: 14,
     title: "The Forest",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/242760/header.jpg",
     hash: 13,
     link: "https://ay.live/gOKy"
   },
   {
     id: 15,
     title: "Sons Of The Forest",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/1326470/header.jpg",
     hash: 14,
     link: "https://ay.live/UTh"
   },
   {
     id: 16,
     title: "Mortal Kombat Komplete Edition",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/237110/header.jpg",
     hash: 15,
     link: "https://ay.live/m0h1"
   },
   {
     id: 17,
     title: "Rise Of The Ronin",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/1340990/header.jpg",
     hash: 16,
     link: "https://ay.live/vHDk"
   },
   {
     id: 18,
     title: "Rise Son Of Rome",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/302510/header.jpg",
     hash: 17,
     link: "https://ay.live/ZXvk"
   },
   {
     id: 19,
     title: "Warhammer Vermintide 2",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/552500/header.jpg",
     hash: 18,
     link: "https://ay.live/OEz"
   },
   {
     id: 20,
     title: "Wo Long Fallen Dynasty",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/1448440/header.jpg",
     hash: 19,
     link: "https://ay.live/TyXV"
   },
   {
     id: 21,
     title: "SCUM",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/513710/header.jpg",
     hash: 20,
     link: "https://ay.live/L04E8"
   },
   {
     id: 22,
     title: "Farming Simulator 2025",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/2300320/header.jpg",
     hash: 21,
     link: "https://ay.live/9wDcwJ"
   },
   {
     id: 23,
     title: "Moto GP 25",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3077390/header.jpg?t=1753692631/header.jpg",
     hash: 22,
     link: "https://ay.live/aoUn"
   },
   {
     id: 24,
     title: "Forza Motorsport",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/2440510/header.jpg",
     hash: 23,
     link: "https://ay.live/Dwwdm"
   },
   {
     id: 25,
     title: "Forza Horizon 4",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/1293830/header.jpg",
     hash: 24,
     link: "https://ay.live/nZyV"
   },
   {
     id: 26,
     title: "Forza Horizon 5",
-    image: "https://cdn.akamai.steamstatic.com/steam/apps//header.jpg",
+    image: "https://cdn.akamai.steamstatic.com/steam/apps/1551360/header.jpg",
     hash: 25,
     link: "https://ay.live/x0cNEc"
   }
